ff7c1dcfd09e43b040d2e9f1aa67f4d1818fb9b4
Yuda Eka <fauzan.risyad@gmail.com>
2026-01-05 14:38:00 +0700

    Add Notifiable trait to User model for notification capabilities

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $guarded = ['id']; // Semua kolom boleh diisi kecuali ID
 
